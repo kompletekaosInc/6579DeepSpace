ddfe5cda373ca22d84189de1519effb64225ab73
Jiah Pang <jiahpang@gmail.com>
2019-02-17 21:35:14 +1100

    Untracked files issue resolved to fix .gitignorev2

--- src/main/java/frc/robot/control/OperatorControls.java
+++ src/main/java/frc/robot/control/OperatorControls.java
@@ -52,7 +52,7 @@ public class OperatorControls extends PSController {
 
     @Override
     protected void processCircle(Robot robot) {
-        robot.getLift().toEncoderHeight(64);
+        robot.getLift().toEncoderHeight(70);
     }
 
     @Override
